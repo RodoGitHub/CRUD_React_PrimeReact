e6e42f0f71dd01979116c13c4ea429c9ed9b8799
Agustin-Invaldi <agustin.invaldi@emprinet.com>
2025-04-07 23:10:19 -0300

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import ObjectsContainer from "./layouts/objects";
+import ProductContainer from "./layouts/Products";
+
+function App() {
+
+  return (
+    <ObjectsContainer />
+
+  );
+}
+
+export default App;
